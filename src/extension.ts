e0855cc0b3d4fa2fbca3b2945be6c7df2fae3125
Parsa Mohammadian <parsa2820@gmail.com>
2025-07-16 12:34:06 -0400

    Fix formatter by adding compilation

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,11 @@ import * as vscode from 'vscode';
 import { LedgerDocumentFormatter } from './formatter';
 
 export function activate(context: vscode.ExtensionContext) {
-  const formatterRegistration = vscode.languages.registerDocumentFormattingEditProvider('ledger', new LedgerDocumentFormatter());
-  context.subscriptions.push(formatterRegistration);
+
+	console.log('Extension "ledger-cli" is now active!');
+
+	const formatterRegistration = vscode.languages.registerDocumentFormattingEditProvider('ledger', new LedgerDocumentFormatter());
+	context.subscriptions.push(formatterRegistration);
 }
+
+export function deactivate() { }
